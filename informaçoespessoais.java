302d0026ca15afc681c2034cf5aabc858317c7ee
MarinaDevs <marinabrito190@gmail.com>
2025-10-21 10:18:11 -0300

    ATV 1 - Informações pessoais
    Esse programa serve para mostrar informações de uma pessoa no console, como nome, idade e altura. Ele usa variáveis para armazenar esses dados e depois exibe tudo em uma única frase. É um exemplo simples de como declarar variáveis e concatenar textos em Java.

---
+++ informaçoespessoais.java
@@ -0,0 +1,10 @@
+public class informaçoespessoais {
+    public static void main(String[] args) {
+        String nome = "Marina";
+        int idade = 15;
+        double altura = 1.68;
+        
+        System.out.print("meu nome é "+ nome + ", tenho " + idade + " anos" + ", tenho " + altura +" m ");
+
+    }
+}
