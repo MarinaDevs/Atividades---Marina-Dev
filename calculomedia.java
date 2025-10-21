a2fe76be35c28b670a5e4ce165a50539261a9dc2
MarinaDevs <marinabrito190@gmail.com>
2025-10-21 10:29:23 -0300

    ATV1 - Calculo média
    Esse código Java tem a função de calcular a média aritmética de três notas (6.5, 8.5 e 9.5). Após o cálculo, ele exibe a média final*na tela.
    Em seguida, ele usa uma estrutura de decisão if/else para verificar se o aluno foi aprovado ou reprovado, considerando que a média mínima para aprovação é 7. Como o cálculo resulta em uma média superior a 7 ( 8.17), o programa conclui com a mensagem Aprovado.

---
+++ calculomedia.java
@@ -0,0 +1,18 @@
+public class calculomedia {
+    public static void main(String[] args) {
+    double nota1= 6.5;
+    double nota2= 8.5;
+    double nota3= 9.5;
+    double media=(nota1+nota2+nota3)/3;
+
+    System.out.println("A media final é:" + media);
+    if(media>=7){
+        System.out.println("Aprovado");
+    }
+    else{
+        System.out.println("Reprovado");
+    }
+
+}
+
+}
