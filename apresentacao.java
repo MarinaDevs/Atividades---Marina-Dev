2c691157dc2bc48cc5da09e10085b773e99ef5fa
MarinaDevs <marinabrito190@gmail.com>
2025-10-21 10:37:12 -0300

    ATV1 - Apresentação
    Esse código, serve apenas para exibir informações pessoais básicas no console. Ele não realiza cálculos ou verificações.
    
    A função principal do programa é apenas imprimir sequencialmente três linhas de texto para fazer uma breve apresentação: o nome, a idade e a cidade onde mora.

---
+++ apresentacao.java
@@ -0,0 +1,8 @@
+public class apresentacao {
+public static void main(String[] args) {
+    System.out.println("meu nome é Marina");
+    System.out.println("tenho 15 anos");
+    System.out.println("moro em Acopiara");  
+}
+ 
+}
