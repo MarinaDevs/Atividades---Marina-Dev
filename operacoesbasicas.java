4d55c39ddbcd6a7ff7a465fac120ccba8691f61a
MarinaDevs <marinabrito190@gmail.com>
2025-10-21 10:26:29 -0300

    ATV1 - Operações básicas
    O programa acima mostra operações básicas para calcular - adição, subtração, multiplicação e divisão.

---
+++ operacoesbasicas.java
@@ -0,0 +1,9 @@
+public class operacoesbasicas {
+    public static void main(String[] args) {
+        int n1=8, n2 = 9;
+        System.out.println(n1+n2);
+        System.out.println(n1-n2);
+        System.out.println(n1*n2);
+        System.out.println(n1/n2);
+    }
+}
