8947b49d5435ce88162878a04c080188c772a30e
MarinaDevs <marinabrito190@gmail.com>
2025-10-21 10:31:48 -0300

    ATV1 - Conversão de dados
    Este código realiza um cálculo simples para converter uma idade em anos para meses e dias.
    
    Ele define uma variável para a idade com o valor 18. Em seguida, ele calcula o equivalente em meses (multiplicando a idade por 12) e em dias (multiplicando a idade por 365, desconsiderando anos bissextos para simplificação).
    
    O objetivo final do programa é apresentar o resultado na tela, mostrando que a idade de 18 anos equivale a 216 meses e 6570 dias.

---
+++ conversaodedados.java
@@ -0,0 +1,8 @@
+public class conversaodedados {
+   public static void main(String[] args) {
+    int idade = 18;
+    int meses = idade * 12;
+    int dias = idade * 365;
+    System.out.println("idade: " + idade + " equivale a: " + meses + " ou dias: " + dias);
+   }
+}
